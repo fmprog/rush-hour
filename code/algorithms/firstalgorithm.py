2198b7c138d06e7f53efbafa4e276b84937cda6d
Fabienne van Baren <fabiennevanbaren@gmail.com>
2022-01-21 12:58:50 +0100

    Adding the first algorithm

---
+++ code/algorithms/firstalgorithm.py
@@ -0,0 +1,63 @@
+import random
+import copy
+
+
+def random_solver(game):
+
+    X = game.vehicles['X']
+    attempt = 1
+    states = {}
+    
+    while X.position[0][1] != game.exit:
+   
+        possibilities_vehicles = list(game.vehicles.keys())
+        select_vehicle = random.choice(possibilities_vehicles)
+
+        possibilities_direction = []
+        if game.vehicles[select_vehicle].orientation == 'H':
+
+            # check if movement to the right is possible
+            front_row_coord = game.vehicles[select_vehicle].position[-1][0]
+            front_col_coord = game.vehicles[select_vehicle].position[-1][1]
+
+            if front_col_coord + 1 != game.size and game.board[front_row_coord][front_col_coord + 1] == 0:
+                possibilities_direction.append('R')
+
+            # check if movement to the left is possible
+            front_row_coord = game.vehicles[select_vehicle].position[0][0]
+            front_col_coord = game.vehicles[select_vehicle].position[0][1]
+
+            if front_col_coord - 1 >= 0 and game.board[front_row_coord][front_col_coord - 1] == 0:
+                possibilities_direction.append('L')
+
+        else:
+
+            # check if movement up is possible
+            front_row_coord = game.vehicles[select_vehicle].position[0][0]
+            front_col_coord = game.vehicles[select_vehicle].position[0][1]
+
+            # check if movement is possible
+            if game.vehicles[select_vehicle].position[0][0] - 1 >= 0 and game.board[front_row_coord - 1][front_col_coord] == 0:
+                possibilities_direction.append('U')
+            
+            # check if movement down is possible
+            front_row_coord = game.vehicles[select_vehicle].position[-1][0]
+            front_col_coord = game.vehicles[select_vehicle].position[-1][1]
+
+            # check if movement is possible
+            if game.vehicles[select_vehicle].position[-1][0] + 1 != game.size and game.board[front_row_coord + 1][front_col_coord] == 0:
+                possibilities_direction.append('D')
+
+        if len(possibilities_direction) > 0:
+            select_direction = random.choice(possibilities_direction)
+
+            # move with selected vehicle to selected direction
+            game.move(select_vehicle, select_direction)
+            print(f"Move: Vehicle {select_vehicle} - Direction {select_direction}")
+            game.show_board()
+            print('-------------------')
+            states[attempt] = game.board
+            attempt += 1
+
+    print(f"Solved the puzzle in {attempt} attempts.")
+    print(states)
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from code.classes.game import Game
 import random
 from code.algorithms import randomise
+from code.algorithms import first_algorithm
 
 
 if __name__ == "__main__":
     # Load game 1
     game = Game("Rushhour6x6_1.csv", 6)
     game.show_board()
-    randomise.random_solver(game)
+    # randomise.random_solver(game)
+    first_algorithm.random_solver(game)
 
