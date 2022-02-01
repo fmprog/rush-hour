caaa594ebf8e953755835b3d94ffac921a330380
Fé Anne Haan <feannehaan@MacBook-Air-van-Fe.local>
2022-02-01 15:44:51 +0100

    added histogram to results.py

--- code/experiment/results.py
+++ code/experiment/results.py
@@ -1,29 +1,52 @@
 import subprocess
 import time
 import csv
+import matplotlib as plt
 
 start = time.time()
 n_runs = 1
 header = []
-results = []
+paths = []
+file = "randomise.py"
 
 while time.time() - start < 3600:
-    print(f"run: {n_runs}")
-    header.append(('Run ') + str(n_runs))
-    print(header)
-    subprocess.call(["timeout", "60", "python3", "randomise.py"])
 
-    result = 0
-    results.append(result)
+    header.append(f'Run {n_runs}')
+
+    subprocess.call([f'code/algorithms/{file}', "timeout", "60", "python3", file])
+
+    path = 0
+    paths.append(path)
 
 
-    with open('countries.csv', 'w', encoding='UTF8', newline='') as f:
+    with open(f'results{file}.csv', 'w', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
 
         # write the header
         writer.writerow(header)
 
         # write multiple rows
-        writer.writerows(results)
+        writer.writerows(paths)
 
     n_runs += 1
+
+    # --------------------------- Hill Climber Algorithm ---------------------
+    results = []
+    for i in range(100):
+        print(i)
+        hill = hill_climber.HillClimber(game, solution)
+        hill.run(100)
+
+        result = hill.solution_len
+        results.append(result)
+
+    print(f"Bord6x6_1 resultaten: {results}")
+    # Make bar plot
+    plt.hist(results, color="blue", bins=range(100))
+
+    plt.xlabel("Lengte Pad")
+    plt.ylabel("Frequentie lengte pad")
+    plt.title("Resultaten Rush Hour Bord 4 9x9")
+
+    # Show graphic
+    plt.savefig("9x9 bord")
