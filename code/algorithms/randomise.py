2d07c2e16fab1c1e6a029b07430fc29d847b8ef0
EmilyTimmerman <90244518+EmilyTimmerman@users.noreply.github.com>
2022-02-02 16:41:56 +0100

    cleanup randomise.py

--- code/algorithms/randomise.py
+++ code/algorithms/randomise.py
@@ -4,7 +4,7 @@ import copy
 
 class Randomise:
     """
-    A Randomiser
+    A Random Algorithm that solves the Rush Hour puzzle by moving random vehicles in random directions until car X is located in front of the exit.
     """
     def __init__(self, game):
         self.current_game = game
@@ -12,34 +12,34 @@ class Randomise:
 
 
     def run(self):
-    
+        """
+        Method to play moves with randomly selected vehicles and direction as long as the puzzle is not solved.
+        """
         while self.current_game.is_solved() == False:
     
-            # randomly select a vehicle to move
+            # Randomly select a vehicle to move
             possibilities_vehicles = list(self.current_game.vehicles.keys())
             select_vehicle = random.choice(possibilities_vehicles)
 
-            # randomly select a direction to move to
+            # Randomly select a direction to move to
             possibilities_direction = self.current_game.possible_direction(select_vehicle)
             
             if len(possibilities_direction) > 0:
                 select_direction = random.choice(possibilities_direction)
 
-                # move the selected vehicle to the selected direction
+                # Create acopy of the game and play the move
                 copy_game = copy.deepcopy(self.current_game)
-
-                # move the vehicle
                 copy_game.move(select_vehicle, select_direction)
                 
-                # create a solution path
+                # Add the move and the resulting board to the solution
                 self.solution.append([copy_game, [select_vehicle, select_direction]])
 
-                # update the current game
+                # Update the current game
                 self.current_game = copy_game
 
 
     def return_solution(self):
-        '''
-        Returns the path and the corresponding boards.
-        '''
+        """
+        Returns all moves and corresponding boards that lead to the solution.
+        """
         return self.solution
